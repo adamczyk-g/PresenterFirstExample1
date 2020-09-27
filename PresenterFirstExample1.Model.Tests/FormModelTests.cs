850b22cdc2ff2b8b7084d41f0a800c7bc2427399
Grzegorz Adamczyk <gadamczyk.net@gmail.com>
2020-09-27 11:22:29 +0200

    test: add class FormModelTests

---
+++ PresenterFirstExample1.Model.Tests/FormModelTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresenterFirstExample1.Model.Tests
+{
+    [TestFixture]
+    public class FormModelTests
+    {
+        
+    }
+}
